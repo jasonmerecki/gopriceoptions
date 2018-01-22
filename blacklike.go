193470a528c2a78b2b230feaf610a5b42d7d1dff
Jason Merecki <rrrrr>
2018-01-21 20:22:59 -0700

    amending for an email

--- blacklike.go
+++ blacklike.go
@@ -52,15 +52,15 @@ func d2f(d1 float64, vt float64) float64 {
 	return d2
 }
 
-func d1pdff (s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+func d1pdff(s float64, k float64, v float64, t float64, r float64, q float64) float64 {
 	vt := (v * (math.Sqrt(t)))
-	d1 := d1f(s, k, t, v, r, q, vt);
-	d1pdf := math.Exp( -(d1 * d1) * 0.5)
-	d1pdf = d1pdf/sqtwopi
+	d1 := d1f(s, k, t, v, r, q, vt)
+	d1pdf := math.Exp(-(d1 * d1) * 0.5)
+	d1pdf = d1pdf / sqtwopi
 	return d1pdf
 }
 
-func BSDelta (otype string, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+func BSDelta(otype string, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
 	var zo float64
 	if "P" == otype {
 		zo = -1
@@ -76,75 +76,75 @@ func BSDelta (otype string, s float64, k float64, t float64, v float64, r float6
 	return delta
 }
 
-func BSVega (s float64, k float64, t float64, v float64, r float64, q float64) float64 {
-	d1pdf := d1pdff(s, k, t, v, r, q)
+func BSVega(s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+	d1pdf := d1pdff(s, k, v, t, r, q)
 	drq := math.Exp(-q * t)
 	sqt := math.Sqrt(t)
 	vega := (d1pdf) * drq * s * sqt * 0.01
 	return vega
 }
 
-func BSGamma  (s float64, k float64, t float64, v float64, r float64, q float64) float64 {
-		drq := math.Exp(-q*t)
-		drd := (s * v * math.Sqrt(t))
-		d1pdf := d1pdff(s, k, v, t, r, q)
-		gamma := (drq / drd)  * d1pdf
-	    return gamma
+func BSGamma(s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+	drq := math.Exp(-q * t)
+	drd := (s * v * math.Sqrt(t))
+	d1pdf := d1pdff(s, k, v, t, r, q)
+	gamma := (drq / drd) * d1pdf
+	return gamma
 }
 
-func BSTheta (otype string, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+func BSTheta(otype string, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
 
-        var sign float64
-		if "P" == otype {
-			sign = -1
-		}
-		if "C" == otype {
-			sign = 1
-		}
-		sqt := math.Sqrt(t)
-		drq := math.Exp(-q*t)
-		dr := math.Exp(-r*t)
-		d1pdf := d1pdff(s, k, v, t, r, q)
-		twosqt := 2 *sqt
-		p1 := -1 * ((s * v * drq)/twosqt) * d1pdf
+	var sign float64
+	if "P" == otype {
+		sign = -1
+	}
+	if "C" == otype {
+		sign = 1
+	}
+	sqt := math.Sqrt(t)
+	drq := math.Exp(-q * t)
+	dr := math.Exp(-r * t)
+	d1pdf := d1pdff(s, k, v, t, r, q)
+	twosqt := 2 * sqt
+	p1 := -1 * ((s * v * drq) / twosqt) * d1pdf
 
-		vt := (v * (sqt))
-		d1 := d1f(s, k, t, v, r, q, vt)
-		d2 := d2f(d1, vt)
-		var nd1, nd2 float64
-		
-		d1 = sign * d1
-		d2 = sign * d2
-		nd1 = Stdnorm.Cdf(d1)
-		nd2 = Stdnorm.Cdf(d2)
-		
-		p2 := -sign * r * k * dr * nd2
-		p3 := sign * q * s * drq * nd1
-		theta := (p1 + p2 + p3) / 365
-		return theta;
+	vt := (v * (sqt))
+	d1 := d1f(s, k, t, v, r, q, vt)
+	d2 := d2f(d1, vt)
+	var nd1, nd2 float64
+
+	d1 = sign * d1
+	d2 = sign * d2
+	nd1 = Stdnorm.Cdf(d1)
+	nd2 = Stdnorm.Cdf(d2)
+
+	p2 := -sign * r * k * dr * nd2
+	p3 := sign * q * s * drq * nd1
+	theta := (p1 + p2 + p3) / 365
+	return theta
 }
 
-func BSRho (otype string, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
-        var sign float64
-		if "P" == otype {
-			sign = -1
-		}
-		if "C" == otype {
-			sign = 1
-		}
-		
-		dr := math.Exp(-r*t)
-		p1 := sign * (k * t * dr) / 100
-		
-		vt := (v * (math.Sqrt(t)))
-		d1 := d1f(s, k, t, v, r, q, vt)
-		d2 := sign * d2f(d1, vt)
-		nd2 := Stdnorm.Cdf(d2)
-		rho := p1 * nd2
-		return rho
+func BSRho(otype string, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+	var sign float64
+	if "P" == otype {
+		sign = -1
+	}
+	if "C" == otype {
+		sign = 1
+	}
+
+	dr := math.Exp(-r * t)
+	p1 := sign * (k * t * dr) / 100
+
+	vt := (v * (math.Sqrt(t)))
+	d1 := d1f(s, k, t, v, r, q, vt)
+	d2 := sign * d2f(d1, vt)
+	nd2 := Stdnorm.Cdf(d2)
+	rho := p1 * nd2
+	return rho
 }
 
-func BSImpliedVol (otype string, p float64, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
+func BSImpliedVol(otype string, p float64, s float64, k float64, t float64, v float64, r float64, q float64) float64 {
 	if v > 0 == false {
 		v = 0.5
 	}
@@ -153,8 +153,8 @@ func BSImpliedVol (otype string, p float64, s float64, k float64, t float64, v f
 	dv := errlim + 1
 	n := 0
 	maxloops := 100
-	
-	for ; math.Abs(dv) > errlim && n < maxl ; n++ {
+
+	for ; math.Abs(dv) > errlim && n < maxl; n++ {
 		difval := PriceBlackScholes(otype, s, k, t, v, r, q) - p
 		v1 := BSVega(s, k, v, t, r, q) / 0.01
 		dv = difval / v1
@@ -169,4 +169,3 @@ func BSImpliedVol (otype string, p float64, s float64, k float64, t float64, v f
 
 	return iv
 }
-
--- blacklike_test.go
+++ blacklike_test.go
@@ -2,8 +2,8 @@ package gopriceoptions
 
 import (
 	"fmt"
+	"math"
 	"testing"
-	
 )
 
 func TestBlackCall1(t *testing.T) {
@@ -39,51 +39,72 @@ func TestBlackPut1(t *testing.T) {
 
 }
 
-
-func TestBSCallGreeks(ts *testing.T) {
+func TestBSCallGreeks(t *testing.T) {
 	otype := "C"
 	s := 1177.62
 	k := 1195.00
-	t := 0.084931506849315 // date 12/19/2017, expiration 1/19/2018, 31 days
+	time := 0.084931506849315 // date 12/19/2017, expiration 1/19/2018, 31 days
 	v := 0.20
 	r := 0.0135
 	q := 0.0
-	delta := BSDelta(otype, s, k, t, v, r, q)
-	gamma := BSGamma(s, k, t, v, r, q)
-	vega := 	BSVega(s, k, t, v, r, q)
-	theta := BSTheta(otype, s, k, t, v, r, q)
-	rho := BSRho(otype, s, k, t, v, r, q)
-	msg := fmt.Sprintf("TestBSCallGreeks, delta %f, gamma %f, vega %f, theta %f, rho %f\n", delta, gamma, vega, theta, rho)
-	// need to double-check Greeks results, they are slightly different from expected
+	delta := BSDelta(otype, s, k, time, v, r, q)
+	gamma := BSGamma(s, k, time, v, r, q)
+	vega := BSVega(s, k, time, v, r, q)
+	theta := BSTheta(otype, s, k, time, v, r, q)
+	rho := BSRho(otype, s, k, time, v, r, q)
+	msg := fmt.Sprintf("TestBSCallGreeks, delta %.24f, gamma %f, vega %f, theta %f, rho %f\n", delta, gamma, vega, theta, rho)
 	edelta := 0.4197454548230388
-	if delta != edelta {
-		ts.Error(msg)
+	egamma := 0.005694
+	evega := 1.341348
+	etheta := -0.450224
+	erho := 0.402579
+	if delta != edelta || math.Abs(egamma - gamma) > 0.00001 || math.Abs(evega - vega) > 0.00001 || math.Abs(etheta - theta) > 0.00001 || math.Abs(erho - rho) > 0.00001 {
+		t.Error(msg)
 	}
 	fmt.Print(msg)
 }
 
-func TestBSPutGreeks(ts *testing.T) {
+func TestBSPutGreeks(t *testing.T) {
 	otype := "P"
 	s := 214.76
 	k := 190.00
-	t := 0.084931506849315 // date 12/19/2017, expiration 1/19/2018, 31 days
+	time := 0.084931506849315 // date 12/19/2017, expiration 1/19/2018, 31 days
 	v := 0.25
 	r := 0.0135
 	q := 0.0
-	delta := BSDelta(otype, s, k, t, v, r, q)
-	gamma := BSGamma(s, k, t, v, r, q)
-	vega := 	BSVega(s, k, t, v, r, q)
-	theta := BSTheta(otype, s, k, t, v, r, q)
-	rho := BSRho(otype, s, k, t, v, r, q)
-	msg := fmt.Sprintf("TestBSPutGreeks, delta %f, gamma %f, vega %f, theta %f, rho %f\n", delta, gamma, vega, theta, rho)
-	// need to double-check Greeks results, they are slightly different from expected
-	// gamma and theta need work
-	edelta := 0.4197454548230388
+	delta := BSDelta(otype, s, k, time, v, r, q)
+	gamma := BSGamma(s, k, time, v, r, q)
+	vega := BSVega(s, k, time, v, r, q)
+	theta := BSTheta(otype, s, k, time, v, r, q)
+	rho := BSRho(otype, s, k, time, v, r, q)
+	msg := fmt.Sprintf("TestBSPutGreeks, delta %.24f, gamma %f, vega %f, theta %f, rho %f\n", delta, gamma, vega, theta, rho)
+	edelta := -0.04150437210202529
+	egamma := 0.005675
+	evega := 0.055574
+	etheta := -0.022069
+	erho := -0.007800
+	if delta != edelta || math.Abs(egamma - gamma) > 0.00001 || math.Abs(evega - vega) > 0.00001 || math.Abs(etheta - theta) > 0.00001 || math.Abs(erho - rho) > 0.00001 {
+		t.Error(msg)
+	}
 	if delta != edelta {
-		ts.Error(msg)
+		t.Error(msg)
 	}
 	fmt.Print(msg)
 }
 
-
-
+func TestBSImpVol(t *testing.T) {
+	otype := "C"
+	p := 20.29616
+	s := 1177.62
+	k := 1195.00
+	time := 0.084931506849315 // date 12/19/2017, expiration 1/19/2018, 31 days
+	r := 0.0135
+	q := 0.0
+	biv := BSImpliedVol(otype, p, s, k, time, 0.0, r, q)
+	msg := fmt.Sprintf("TestBSImpVol, implied vol %f \n", biv)
+	diff := math.Abs(biv - 0.20)
+	if diff > 0.00001 {
+		t.Error(msg)
+	}
+	fmt.Print(msg)
+}
